66fb57e516ff7d0141468a7cd5c5a2f7e2ff2e89
Max Moeller <maximomoeller@gmail.com>
2025-08-21 01:49:24 +1000

    feat(Costumer): Define Costumer Model and Schemas

---
+++ models/costumer.py
@@ -0,0 +1,25 @@
+from init import db
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+
+
+# Costumer Model
+class Costumer(db.Model):
+    # Name of the table
+    __tablename__ = "Costumer" 
+    # Attributes
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    contact = db.Column(db.String(20))
+
+
+class CostumerSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Costumer
+        load_instance = True
+
+# Schema for single entry
+costumer_schema = CostumerSchema()
+
+# Schema for multiple entries
+costumers_schema = CostumerSchema(many=True)
